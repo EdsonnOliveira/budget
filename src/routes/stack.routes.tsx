6d1451ed3e26e081b44264362fa3157437636f80
Edson.Talent <edson.talent@fleetcor.com.br>
2023-01-20 17:16:23 -0300

    GestureEnabled in Routes

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -21,20 +21,21 @@ const AppRoutes: React.FC = () => (
     <stackRoutes.Navigator
         initialRouteName='Home'
         screenOptions={{
-            gestureEnabled: false,
+            gestureEnabled: true,
             headerShown: false,
         }}
     >
-        <stackRoutes.Screen name='Home' component={Home} />
-        <stackRoutes.Screen name='Scanner' component={Scanner} />
+        <stackRoutes.Screen name='Home' component={Home} options={{ gestureEnabled: false }} />
 
-        <stackRoutes.Screen name='Details' component={Details} />
-        <stackRoutes.Screen name='DetailsModify' component={DetailsModify} />
-        <stackRoutes.Screen name='DetailsFinished' component={Finished} />
+        <stackRoutes.Screen name='Scanner' component={Scanner} options={{ gestureEnabled: true }} />
 
-        <stackRoutes.Screen name='Products' component={Products} />
-        <stackRoutes.Screen name='ProductsAdd' component={ProductsAdd} />
-        <stackRoutes.Screen name='ProductsModify' component={ProductsModify} />
+        <stackRoutes.Screen name='Details' component={Details} options={{ gestureEnabled: false }} />
+        <stackRoutes.Screen name='DetailsModify' component={DetailsModify} options={{ gestureEnabled: true }} />
+        <stackRoutes.Screen name='DetailsFinished' component={Finished} options={{ gestureEnabled: false }} />
+
+        <stackRoutes.Screen name='Products' component={Products} options={{ gestureEnabled: false }} />
+        <stackRoutes.Screen name='ProductsAdd' component={ProductsAdd} options={{ gestureEnabled: true }} />
+        <stackRoutes.Screen name='ProductsModify' component={ProductsModify} options={{ gestureEnabled: true }} />
     </stackRoutes.Navigator>
 )
 
