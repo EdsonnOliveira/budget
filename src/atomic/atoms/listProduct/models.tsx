92dfe878b95cd721d4fc341bda38edcd8b229407
Edson.Talent <edson.talent@fleetcor.com.br>
2023-01-23 23:07:03 -0300

    Correction props

--- src/atomic/atoms/listProduct/models.tsx
+++ src/atomic/atoms/listProduct/models.tsx
@@ -1,6 +1,6 @@
 import { ItemType } from "../../../constants/types";
 
 export interface IndexProps {
-    data: ItemType[];
+    data: ItemType[] | null;
     onPress?: (data: ItemType, index: number) => void;
 }
