783542d97122e3c395e3b1165cacd330f167cc6a
Edson.Talent <edson.talent@fleetcor.com.br>
2023-02-03 14:46:59 -0300

    New Fonts

--- src/atomic/atoms/boxes/boxCommon/index.tsx
+++ src/atomic/atoms/boxes/boxCommon/index.tsx
@@ -8,6 +8,7 @@ const BoxCommon = styled.View`
     height: ${( props: IndexStyledProps ) => ( props.height ? props.height : 'null' )};
     flex: ${( props: IndexStyledProps ) => ( props.flex ? 1 : 'none' )};
     background-color: ${( props: IndexStyledProps ) => ( props.bgColor ?? 'transparent' )};
+
     flex-direction: ${( props: AlignmentsStyledProps ) => ( props.flexDirection ?? 'column' )};
     justify-content: ${( props: AlignmentsStyledProps ) => ( props.justifyContent ?? 'flex-start' )};
     align-items: ${( props: AlignmentsStyledProps ) => ( props.alignItems ?? 'flex-start' )};
